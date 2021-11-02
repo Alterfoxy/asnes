ac92c4aecc50fa8174e46967c523754a27d1e4b2
FoxLib <hplus.foxtail@gmail.com>
2021-11-02 16:21:59 +0300

    Добавлено еще инструкции

--- alu.v
+++ alu.v
@@ -5,7 +5,6 @@ module alu
     input  wire [7:0] A,        // Значение src
     input  wire [7:0] B,        // Значение dst
     input  wire [7:0] P,        // Флаги на вход
-    input  wire [7:0] opcode,
 
     // Результат
     output wire [7:0] AR,       // Результат
@@ -25,31 +24,37 @@ wire oadc  = (A[7] ^ B[7] ^ 1'b1) & (A[7] ^ R[7]); // Переполнение A
 wire osbc  = (A[7] ^ B[7] ^ 1'b0) & (A[7] ^ R[7]); // Переполнение SBC
 wire cin   =  P[0];
 
+localparam
+    ORA = 4'b0000, AND = 4'b0001, EOR = 4'b0010, ADC = 4'b0011,
+    STA = 4'b0100, LDA = 4'b0101, CMP = 4'b0110, SBC = 4'b0111,
+    ASL = 4'b1000, ROL = 4'b1001, LSR = 4'b1010, ROR = 4'b1011,
+    BIT = 4'b1100, DEC = 4'b1101, INC = 4'b1110;
+
 always @* begin
 
     // Расчет результата
     case (ALU)
 
         // Общие
-        /* ORA */ 4'b0000: R = A | B;
-        /* AND */ 4'b0001: R = A & B;
-        /* EOR */ 4'b0010: R = A ^ B;
-        /* ADC */ 4'b0011: R = A + B + cin;
-        /* STA */ 4'b0100: R = A;
-        /* LDA */ 4'b0101: R = B;
-        /* CMP */ 4'b0110: R = A - B;
-        /* SBC */ 4'b0111: R = A - B - !cin;
+        ORA: R = A | B;
+        AND: R = A & B;
+        EOR: R = A ^ B;
+        ADC: R = A + B + cin;
+        STA: R = A;
+        LDA: R = B;
+        CMP: R = A - B;
+        SBC: R = A - B - !cin;
 
         // Сдвиги
-        /* ASL */ 4'b1000: R = {B[6:0], 1'b0};
-        /* ROL */ 4'b1001: R = {B[6:0], P[0]};
-        /* LSR */ 4'b1010: R = {1'b0, B[7:1]};
-        /* ROR */ 4'b1011: R = {P[0], B[7:1]};
+        ASL: R = {B[6:0], 1'b0};
+        ROL: R = {B[6:0], P[0]};
+        LSR: R = {1'b0, B[7:1]};
+        ROR: R = {P[0], B[7:1]};
 
         // Специальные
-        /* BIT */ 4'b1100: R = A & B;
-        /* DEC */ 4'b1101: R = B - 1;
-        /* INC */ 4'b1110: R = B + 1;
+        BIT: R = A & B;
+        DEC: R = B - 1;
+        INC: R = B + 1;
 
     endcase
 
@@ -57,16 +62,15 @@ always @* begin
     casex (ALU)
 
         // ORA, AND, EOR, LDA, STA, INC, DEC
-        4'b000x, 4'b0010, 4'b010x, 4'b111x:
-                                 AF = {sign,        P[6:2], zero, P[0]};
-        /* ADC */       4'b0011: AF = {sign, oadc,  P[5:2], zero,  cout};
-        /* CMP */       4'b0110: AF = {sign,        P[6:2], zero, ~cout};
-        /* SBC */       4'b0111: AF = {sign, osbc,  P[5:2], zero, ~cout};
-        /* ASL, ROL */  4'b100x: AF = {sign,        P[6:2], zero,  B[7]};
-        /* LSR, ROR */  4'b101x: AF = {sign,        P[6:2], zero,  B[0]};
-
-        // BIT
-        4'b1100: AF = {B[7:6], P[5:2], zero, P[0]};
+        4'b000x, 4'b0010,
+        4'b010x, 4'b111x:
+                    AF = {sign,        P[6:2], zero,  P[0]};
+        ADC:        AF = {sign, oadc,  P[5:2], zero,  cout};
+        CMP:        AF = {sign,        P[6:2], zero, ~cout};
+        SBC:        AF = {sign, osbc,  P[5:2], zero, ~cout};
+        ASL, ROL:   AF = {sign,        P[6:2], zero,  B[7]};
+        LSR, ROR:   AF = {sign,        P[6:2], zero,  B[0]};
+        BIT:        AF = {B[7:6],      P[5:2], zero,  P[0]};
 
     endcase
 
--- cpu.v
+++ cpu.v
@@ -51,6 +51,8 @@ case (T)
 
         // Обработка некоторых опкодов
         casex (i_data)
+
+        // Флаги
         /* CLC */ 8'h18: begin T <= RST; P[0] <= 1'b0; end
         /* SEC */ 8'h38: begin T <= RST; P[0] <= 1'b1; end
         /* CLI */ 8'h58: begin T <= RST; P[2] <= 1'b0; end
@@ -58,6 +60,30 @@ case (T)
         /* CLV */ 8'hB8: begin T <= RST; P[6] <= 1'b0; end
         /* CLD */ 8'hD8: begin T <= RST; P[3] <= 1'b0; end
         /* SED */ 8'hF8: begin T <= RST; P[3] <= 1'b1; end
+
+        // Пересылка
+        /* TYA */ 8'h98: begin T <= RST; A <= Y; P <= {Y[7], P[6:2], Y==0, P[0]}; end
+        /* TAY */ 8'hA8: begin T <= RST; Y <= A; P <= {A[7], P[6:2], A==0, P[0]}; end
+        /* TXA */ 8'h8A: begin T <= RST; A <= X; P <= {X[7], P[6:2], X==0, P[0]}; end
+        /* TAX */ 8'hAA: begin T <= RST; X <= A; P <= {A[7], P[6:2], A==0, P[0]}; end
+        /* TSX */ 8'hBA: begin T <= RST; X <= S; P <= {S[7], P[6:2], S==0, P[0]}; end
+        /* TXS */ 8'h9A: begin T <= RST; S <= X; end
+        /* NOP */ 8'hEA, 8'h1A, 8'h3A, 8'h5A, 8'h7A, 8'hDA, 8'hFA: T <= RST;
+
+        // Выбор АЛУ
+        /* LDXY*/ 8'hA0, 8'hA4, 8'hAC, 8'hB4, 8'hBC,
+                  8'hA2, 8'hA6, 8'hAE, 8'hB6, 8'hBE: alu <= /* LDA */ 4'b0101;
+        /* CPX */ 8'hE0, 8'hE4, 8'hEC: begin alu <= /* CMP */ 4'b0110; src <= 2'h1; end
+        /* CPY */ 8'hC0, 8'hC4, 8'hCC: begin alu <= /* CMP */ 4'b0110; src <= 2'h2; end
+
+        // Однотактовая работа сдвиговых инструкции
+        /* ASL */ 8'h0A: begin A <= {A[6:0], 1'b0}; P <= {A[6], P[6:2],  A[6:0]==0,       A[7]}; T <= RST; end
+        /* ROL */ 8'h2A: begin A <= {A[6:0], P[0]}; P <= {A[6], P[6:2], {A[6:0],P[0]}==0, A[7]}; T <= RST; end
+        /* LSR */ 8'h4A: begin A <= {1'b0, A[7:1]}; P <= {1'b0, P[6:2],  A[7:1]==0,       A[0]}; T <= RST; end
+        /* ROR */ 8'h6A: begin A <= {P[0], A[7:1]}; P <= {P[0], P[6:2], {P[0],A[7:1]}==0, A[0]}; T <= RST; end
+
+        // ASL, ROL, LSR, ROR Memory
+        8'b0xx_xxx10: begin alu <= {1'b1, i_data[7:5]}; end
         endcase
 
     end
@@ -105,6 +131,9 @@ case (T)
 
     end
 
+    // Завершение записи и выход
+    WEND: begin we <= 1'b0; sel <= 1'b0; T <= RST; end
+
     // -----------------------------------------------------------------
 
     // Исполнение инструкции
@@ -114,21 +143,30 @@ case (T)
         // Специальный случай (требуется PC+1)
         if (T == IMM) pc <= pc + 1'b1;
 
+        T   <= RST;
+        sel <= 1'b0;
+
         casex (opcode)
 
-            // STA x
-            8'b100_xxx_01: case (T)
+            // ST(A|X|Y)
+            8'b100_xxx_01: begin T <= WEND; we <= 1'b1; sel <= 1'b1; o_data <= src_mux; end
 
-                IMM, IMP: begin we <= 1'b1; T <= TICK1; o_data <= A; end
-                TICK1:    begin we <= 1'b0; sel <= 1'b0; end
+            // ASL, LSR, ROL, ROR Imm
+            8'b0xx_xxx_10: begin T <= WEND; we <= 1'b1; sel <= 1'b1; o_data <= alu_r; P <= alu_r;  end
 
-            endcase
+            // Стандартное АЛУ. Запись в Acc, если не CMP
+            8'bxxx_xxx_01: begin P <= alu_p; if (opcode[7:5] != 3'b110) A <= alu_r; end
 
-            // Стандартное АЛУ
-            8'bxxx_xxx_01: begin A <= alu_r; P <= alu_p; sel <= 1'b0; end
+            // LDX, LDY
+            8'hA2, 8'hA6, 8'hAE, 8'hB6, 8'hBE: begin X <= alu_r; P <= alu_p; end
+            8'hA0, 8'hA4, 8'hAC, 8'hB4, 8'hBC: begin Y <= alu_r; P <= alu_p; end
+
+            // CPX, CPY
+            8'hC0, 8'hC4, 8'hCC,
+            8'hE0, 8'hE4, 8'hEC: P <= alu_p;
 
             // Неопознанная инструкция
-            default: begin T <= RST; sel <= 1'b0; end
+            default: begin sel <= 1'b0; end
 
         endcase
 
